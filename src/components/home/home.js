c0fa4349a2e1f3ad05a82b6d220cdc9839dc21c5
João Vitor <122707909+Vitor104@users.noreply.github.com>
2024-08-21 20:06:08 -0300

    estou começando a mexer com o API

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,14 +1,23 @@
 import styles from './home.module.css';
 import Nav from '../nav/nav';
 import Topics from '../topics/topics';
+import { getSubreddits } from '../api/api';
 
 function Home () {
+
+    const subreddits = getSubreddits;
     return (
         <div className={styles.homeContainer}>
             <Nav />
-            
             <div className={styles.contents}>
-                <h1>ola</h1>
+                <ul>
+                {subreddits.map((subreddit) => (
+          <li
+            key={subreddit.id}
+          >
+            </li>
+                )}
+                </ul>
             </div>
             <Topics />
         </div>  
